d3063f70065f809fe5414e5163027caf3bb977d9
Pustovalov95 <88744673+Pustovalov95@users.noreply.github.com>
2021-12-23 11:48:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: false,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/programs/icons/left_arrow.png" alt=""> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="img/programs/icons/right_arrow.png" alt=""> </button>',
+        responsive: [
+            {
+              breakpoint: 1023,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+  });
+
+  let   burger = document.querySelector('.header__navbar-burger'),
+        menu = document.querySelector('.menu');
+
+    function ToggleMenu() {
+        menu.classList.toggle('active');
+        burger.classList.toggle('active');
+    }
+
+
+
+    burger.addEventListener('click', () => {
+        ToggleMenu();
+    });
